88632faf68fd6a4c198df548fbbcfbf982f73184
Radagast-dev <57732837+Radagast-dev@users.noreply.github.com>
2024-10-25 13:44:00 +0200

    Add project files.

---
+++ ToDo Manager/App.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace ToDo_Manager
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+
+    }
+
+}
