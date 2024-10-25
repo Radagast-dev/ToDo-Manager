45642a66b7dfe88bbf6dbe162658be449a0fc427
Radagast-dev <57732837+Radagast-dev@users.noreply.github.com>
2024-10-25 16:55:47 +0200

    LoginWindow, versucht zur LFZ zu ändern um Ladezeit + Anmeldung mit kommender DB zu Überbrücken -> Funktioniert noch nicht richtig

--- ToDo Manager/LoginWindow.xaml.cs
+++ ToDo Manager/LoginWindow.xaml.cs
@@ -31,9 +31,33 @@ namespace ToDo_Manager
         {
             if (pwBox.Password == testPW) //implement db connection and account management
             {
+
+                ImageBrush backGroundBrush = new ImageBrush();
+
+                BitmapImage image = new BitmapImage();
+                image.BeginInit();
+                image.UriSource = new Uri("C:\\Users\\xx\\source\\repos\\ToDo Manager\\ToDo Manager\\ressources\\game-icon.png");
+                image.EndInit();
+
+                backGroundBrush.ImageSource = image;
+
+                LoginWindow loginWindow = new LoginWindow();
+                loginWindow.Background = backGroundBrush;
+                loginWindow.pwLabel.Visibility = Visibility.Collapsed;
+                loginWindow.pwBox.Visibility = Visibility.Collapsed;
+                loginWindow.loginEnterButton.Visibility = Visibility.Collapsed;
+                loginWindow.registerbutton.Visibility = Visibility.Collapsed;
+                loginWindow.enterRect.Visibility = Visibility.Collapsed;
+                loginWindow.hoI_IV_Icon.Visibility = Visibility.Visible;
+                loginWindow.Show();
+
+                Thread.Sleep(3000);
+                //obiger block funktioniert nicht richtig, es wird ein blankes login fenster geladen ohne bildhintergrund,
+                //dann folgt Main-Fenster und dann wird im hintergrund erst ein weiters login-fenster geöffnet mit den obigen code-änderungen
+
                 MainWindow mainWindow = new MainWindow();
                 mainWindow.Show();
-                this.Close();
+
             }
             else
             {
@@ -43,7 +67,7 @@ namespace ToDo_Manager
 
         private void registerbutton_Click(object sender, RoutedEventArgs e)
         {
-            //db connection and account mngmt
+            MessageBox.Show("Implement Account Management\nImplement Database");//db connection and account mngmt
         }
     }
 }
